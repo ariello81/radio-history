3a800a797049721c39349e20a31655fd27d5df83
ariello81 <arielr@o2.pl>
2023-01-08 14:08:01 +0100

    dodano pakiet data-jpa, klase StationEntity i intefejs StationRepository

--- src/main/java/pl/ryzykowski/radiohistory/entity/Station.java
+++ src/main/java/pl/ryzykowski/radiohistory/entity/Station.java
@@ -1,4 +1,38 @@
 package pl.ryzykowski.radiohistory.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private String id;
+    @Column(name="name")
+    private String name;
+
+    public Station() {
+    }
+
+    public Station(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/main/java/pl/ryzykowski/radiohistory/repository/StationRepository.java
@@ -0,0 +1,7 @@
+package pl.ryzykowski.radiohistory.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.ryzykowski.radiohistory.entity.Station;
+
+public interface StationRepository extends JpaRepository<Station, Long> {
+}
